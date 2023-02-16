0d826c2644e121508c4083b17b539ba30eab059a
LuisAlegriaT <112205532+LuisAlegriaT@users.noreply.github.com>
2023-02-15 20:33:51 -0600

    Preparación del entrono virtual

---
+++ macuinDash.py
@@ -0,0 +1,25 @@
+#importar el framework 
+from colorama import Cursor
+from flask import Flask, render_template, request ,redirect,url_for,flash 
+from flask_mysqldb import MySQL
+
+#inicializar variable usar flask
+app= Flask(__name__, template_folder='views')
+
+#configuracion de la conexion 
+app.config['MYSQL_HOST']= 'localhost'
+app.config['MYSQL_USER']= 'root'
+app.config['MYSQL_PASSWORD']= ''
+app.config['MYSQL_DB']= 'BDMacuin'
+mysql= MySQL(app)
+app.secret_key='mysecretkey'
+
+@app.route('/')
+def login():
+    return render_template('adminClandAx.html')
+
+#Arrancamos servidor
+if __name__ == '__main__':
+    app.run(port=3000,debug= True)
+
+
